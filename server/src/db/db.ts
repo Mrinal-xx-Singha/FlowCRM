c6d00cb08b8a105fd03b0f7f00fec089372c91ae
Mrinal Singha <mrinalsingha17@gmail.com>
2026-04-24 13:58:00 +0000

    connected the database added a schema

---
+++ server/src/db/db.ts
@@ -0,0 +1,25 @@
+import pkg from "pg"
+const {Pool} = pkg
+
+export const pool = new Pool({
+    connectionString:process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized:false
+    }
+})
+
+
+export const connectDB = async() =>{
+    try {
+        const client = await pool.connect()
+        console.log("✅ PostgresSql connected")
+
+        await client.query("SELECT 1")
+        client.release()
+    } catch (error) {
+        console.error("❌ PostgreSQL connection failed",error)
+        process.exit(1)
+    }
+}
+
+
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,26 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+import { connectDB } from "./db/db";
+import authRoutes from "./routes/auth.route"
+
 const app = express();
+app.use(express.json())
+
+
 
 app.get("/",(req,res)=>{
     res.send("API running")
 })
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on PORT: ${PORT}`);
-});
+app.use("/auth",authRoutes)
+
+const startServer = async() =>{
+    await connectDB()
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+        console.log(`Server running on PORT: ${PORT}`);
+    });
+}
+
+startServer()
